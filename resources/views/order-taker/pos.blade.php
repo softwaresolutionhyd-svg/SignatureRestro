34eecc9be3e1708d917c359e13d716be19250cc5
softwaresolutionhyd-svg <softwaresolutionhyd@gmail.com>
2026-07-15 16:25:18 +0500

    Auto deploy 2026-07-15 16:25

--- resources/views/order-taker/pos.blade.php
+++ resources/views/order-taker/pos.blade.php
@@ -8,7 +8,7 @@
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700;800&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="{{ asset('css/restaurant-pos.css') }}?v=46">
-<link rel="stylesheet" href="{{ asset('css/order-taker-pos.css') }}?v=8">
+<link rel="stylesheet" href="{{ asset('css/order-taker-pos.css') }}?v=9">
 @endpush
 
 @section('content')
